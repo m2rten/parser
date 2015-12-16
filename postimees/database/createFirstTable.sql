981db87da3b6c669dd1823a60a527c271112a82d
marten ester <marten ester>
2015-12-16 13:53:50 +0200

    added database code

---
+++ postimees/database/createFirstTable.sql
@@ -0,0 +1,10 @@
+﻿drop table headings;
+
+create table headings(
+heading varchar (200),
+rank int,
+date date ,
+time time
+);
+--insert into headings values ('Ilves:Eesti piirialade venelased meile ohtu ei kujuta',1,'12/12/2015','21:01:55');
+--delete from headings ; 
